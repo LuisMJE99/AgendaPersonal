975bfb5d834fb88891384f27ac46ce788b553702
luis <luismanu_jimenez@hotmail.com>
2022-02-18 13:42:04 -0700

    Detalles corregidos

--- index.php
+++ index.php
@@ -37,10 +37,7 @@
          
          
         </ul>
-        <form class="form-inline my-2 my-lg-0">
-          <input class="form-control mr-sm-2" type="text" placeholder="buscar" aria-label="Search">
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
-        </form>
+        
       </div>
     </nav>
 
@@ -137,7 +134,7 @@
               "decimal": "",
           "emptyTable": "No hay información",
           "info": "Mostrando _START_ a _END_ de _TOTAL_ Entradas",
-          "infoEmpty": "Mostrando 0 to 0 of 0 Entradas",
+          "infoEmpty": "Mostrando 0 a 0 de 0 Entradas",
           "infoFiltered": "(Filtrado de _MAX_ total entradas)",
           "infoPostFix": "",
           "thousands": ",",
